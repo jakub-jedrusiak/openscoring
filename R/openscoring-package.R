6af6fbdc5ccf37f07f997ae29718be0751a4b453
Jakub Jędrusiak <40692851+jakub-jedrusiak@users.noreply.github.com>
2023-11-06 21:10:28 +0100

    dependencies

--- R/openscoring-package.R
+++ R/openscoring-package.R
@@ -2,5 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import rlang
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
+#' @importFrom rlang !!
+#' @importFrom rlang enquo
 ## usethis namespace: end
 NULL
