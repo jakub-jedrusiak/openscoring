a9de1b2ab1386e085818f6061daebbe1510741cc
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-01-31 20:00:49 +0100

    debug error codes, add quiet

--- R/oscai.R
+++ R/oscai.R
@@ -8,52 +8,56 @@
 #' @param answer The column name of the responses. Commas will be replaced with spaces for scoring.
 #' @param model The model to use. Can be one of "chatgpt", "babbage2", "davinci2".
 #' @param scores_col The column name to store the scores in. Defaults to ".originality".
+#' @param quiet Whether to print the citation reminder.
 #'
 #' @return The input data frame with the scores added.
-#' 
+#'
 #' @examples
 #' df <- data.frame(
 #'   stimulus = c("brick", "hammer", "sponge"),
 #'   response = c("butter for trolls", "make Thor jealous", "make it play in a kids show")
 #' )
-#' 
+#'
 #' df <- oscai(df, stimulus, response, model = "davinci2")
 #'
 #' @export
 
-oscai <- function(df, item, answer, model = c("chatgpt", "babbage2", "davinci2"), scores_col = ".originality") {
-    item <- rlang::ensym(item)
-    answer <- rlang::ensym(answer)
-    model <- rlang::arg_match(model)
+oscai <- function(df, item, answer, model = c("chatgpt", "babbage2", "davinci2"), scores_col = ".originality", quiet = FALSE) {
+  item <- rlang::ensym(item)
+  answer <- rlang::ensym(answer)
+  model <- rlang::arg_match(model)
 
-    model <- switch(model,
-        chatgpt = "ocsai-chatgpt",
-        babbage2 = "ocsai-babbage2",
-        davinci2 = "ocsai-davinci2"
+  model <- switch(model,
+    chatgpt = "ocsai-chatgpt",
+    babbage2 = "ocsai-babbage2",
+    davinci2 = "ocsai-davinci2"
+  )
+
+  item <- df[[rlang::as_label(item)]]
+  answer <- df[[rlang::as_label(answer)]]
+
+  item <- stringr::str_replace_all(item, ",", " ")
+  answer <- stringr::str_replace_all(answer, ",", " ")
+  input <- paste0("\"", item, "\", \"", answer, "\"", collapse = "\n")
+
+  res <- httr::GET(
+    "https://openscoring.du.edu/llm",
+    query = list(
+      model = model,
+      input = input
     )
+  )
 
-    item <- df[[rlang::as_label(item)]]
-    answer <- df[[rlang::as_label(answer)]]
+  if (res$status_code != 200) {
+    cli::cli_abort("OpenScoring API returned status code {res$status_code}\n\n{res}")
+  }
 
-    item <- stringr::str_replace_all(item, ",", " ")
-    answer <- stringr::str_replace_all(answer, ",", " ")
-    input <- paste0("\"", item, "\", \"", answer, "\"", collapse = "\n")
+  content <- jsonlite::fromJSON(rawToChar(res$content))
 
-    res <- httr::GET(
-        "https://openscoring.du.edu/llm",
-        query = list(
-            model = model,
-            input = input
-        )
-    )
-    content <- jsonlite::fromJSON(rawToChar(res$content))
-
-    if (res$status_code != 200) {
-        cli::cli_abort("OpenScoring API returned status code {res$status_code}")
-    }
-
+  if (!quiet) {
     cli::cli_inform(c("v" = "Remember to cite:\n\n{content$cite}"))
-    df[[scores_col]] <- content$scores$originality
+  }
+  df[[scores_col]] <- content$scores$originality
 
-    return(df)
+  return(df)
 }
